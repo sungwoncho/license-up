10ece7974b37555de00117ccbb1bfb08470c3766
Sung Won Cho <mikeswcho@gmail.com>
2016-01-10 10:06:24 +1100

    Implement update-license module for MIT licenses

--- test/update-license-test.js
+++ test/update-license-test.js
@@ -3,18 +3,16 @@ var expect = require('chai').expect;
 
 var updateLicense = require('../lib/update-license');
 
-var MIT_2015 = fs.readFileSync('./test/fixtures/MIT/2015.txt', {encoding: 'utf8'});
-var MIT_2012_2015 = fs.readFileSync('./test/fixtures/MIT/2012_2015.txt', {encoding: 'utf8'});
-var MIT_2016 = fs.readFileSync('./test/fixtures/MIT/2016.txt', {encoding: 'utf8'});
-var MIT_2012_2016 = fs.readFileSync('./test/fixtures/MIT/2012_2016.txt', {encoding: 'utf8'});
-
 describe("update-license", function() {
-  it("updates 2015 to 2016", function() {
-    var result = updateLicense(MIT_2015);
-    expect(result).to.equal(MIT_2016);
+  it("updates 2015 to 2015-2016", function() {
+    var yearInfo = 'Copyright (c) 2015 Sung Won Cho';
+    var result = updateLicense(yearInfo);
+    expect(result).to.equal('Copyright (c) 2015-2016 Sung Won Cho');
   });
 
   it("updates 2012-2015 to 2012-2016", function() {
-    //TODO
+    var yearInfo = 'Copyright (c) 2012-2015 Sung Won Cho';
+    var result = updateLicense(yearInfo);
+    expect(result).to.equal('Copyright (c) 2012-2016 Sung Won Cho');
   });
 });
