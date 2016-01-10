fc2ac5214666a49af129be94b6cad7e188fe2a2c
Sung Won Cho <mikeswcho@gmail.com>
2016-01-10 11:24:34 +1100

    Fix test using new asynchronous API

--- test/update-license-test.js
+++ test/update-license-test.js
@@ -4,15 +4,19 @@ var expect = require('chai').expect;
 var updateLicense = require('../lib/update-license');
 
 describe("update-license", function() {
-  it("updates 2015 to 2015-2016", function() {
+  it("updates 2015 to 2015-2016", function(done) {
     var yearInfo = 'Copyright (c) 2015 Sung Won Cho';
-    var result = updateLicense(yearInfo);
-    expect(result).to.equal('Copyright (c) 2015-2016 Sung Won Cho');
+    var result = updateLicense(yearInfo, function (err, updatedLicenseContent) {
+      expect(updatedLicenseContent).to.equal('Copyright (c) 2015-2016 Sung Won Cho');
+      done();
+    });
   });
 
-  it("updates 2012-2015 to 2012-2016", function() {
+  it("updates 2012-2015 to 2012-2016", function(done) {
     var yearInfo = 'Copyright (c) 2012-2015 Sung Won Cho';
-    var result = updateLicense(yearInfo);
-    expect(result).to.equal('Copyright (c) 2012-2016 Sung Won Cho');
+    var result = updateLicense(yearInfo, function (err, updatedLicenseContent) {
+      expect(updatedLicenseContent).to.equal('Copyright (c) 2012-2016 Sung Won Cho');
+      done();
+    });
   });
 });
